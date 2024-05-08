4ad6e0439beeeb2bfae25571e2a9fbe093051cc6
Phathumzi <phathumzindlovu@gmail.com>
2024-05-08 07:15:48 -0700

    chec

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# To compile all .c files in the current directory into object files run:
-gcc -c -fPIC *.c
-# Create the dynamic library liball.so from the object files
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
-# Then remove the object files
-rm *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
--- 0x18-dynamic_libraries/README.md
+++ 0x18-dynamic_libraries/README.md
@@ -1,1 +1,1 @@
-dymic libraries
+0x18. C - Dynamic libraries

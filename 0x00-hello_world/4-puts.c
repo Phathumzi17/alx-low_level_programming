674fe76eb8a0bb54cbcc6cb6b9c923f4d71c994e
phathumzi <phathumzindlovu@gmail.com>
2024-06-28 02:14:43 -0700

    idk anymore

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+#include<stdio.h>i
+int main(void)
+{
+	char Mystr[]  =  "\"Programming is like building a multilingual puzzle";
 
-int main(void){
-	char Mystr[]="\"Programming is like building a multilingual puzzle";
-	puts(Mystr); 
+	puts(Mystr);
 
-
-return 0;
+	return (0);
 }

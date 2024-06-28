2fe1ac7b9fed784a88c721e89bb19d67260a5e6f
phathumzi <phathumzindlovu@gmail.com>
2024-06-28 02:20:32 -0700

    idk anymore3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This function prints a string to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char Mystr[]  =  "\"Programming is like building a multilingual puzzle";

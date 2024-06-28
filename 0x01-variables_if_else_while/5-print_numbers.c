b100d3b0020b95e62807b5ba36d61d954a01499c
phathumzi <phathumzindlovu@gmail.com>
2024-06-28 04:50:11 -0700

    not sure

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the digits from 0 to 9,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+
+	return (0);
+}

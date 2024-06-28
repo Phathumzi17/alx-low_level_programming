5ced6720ff8fa34736af5e4f1d13ef34b4a2e461
phathumzi <phathumzindlovu@gmail.com>
2024-06-28 03:38:21 -0700

    only warnings

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,33 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: This program tells if a random number is pos/neg/zero
- * it is compiled and run on.
+ * Description: This program tells if a random number is positive, negative,
+ * or zero. It is compiled and run on.
  *
  * Return: Always 0 (Success)
- * betty style doc for function main goes there */
+ */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    if (n > 0)
+    {
+	    printf("%d is positive\n", n);
+    }
+    else if (n == 0)
+    {
+	    printf("%d is zero\n", n);
+    }
+    else if (n < 0)
+    {
+	    printf("%d is negative\n", n);
+    }
 
-	if(n > 0){
-	printf("%d is positive\n",n);
-	}
-
-	else if(n == 0){
-	printf("%d is zero\n",n);
-	}
-
-	else if (n < 0){
-	printf("%d is negative\n",n);
-	}
-
-	return (0);
+    return (0);
 }

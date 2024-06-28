fe4b5f88c7e127aa3db42f9314869f7df7e9428b
phathumzi <phathumzindlovu@gmail.com>
2024-06-28 02:36:46 -0700

    again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
-/*Use the printf function*/
+/**
+* main -Entry point
+* Discription: Use the printf function
+* Return: Always 0(Sucess)
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
